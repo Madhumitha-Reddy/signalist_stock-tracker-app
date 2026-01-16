3e461e1172d0b946019a9aee318ec11b3adecec3
Madhumitha-Reddy <madhumithareddyjinna@gmail.com>
2026-01-16 18:08:18 +0530

    Implement Daily new summary

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,5 +1,5 @@
 import nodemailer from 'nodemailer';
-import {WELCOME_EMAIL_TEMPLATE} from "@/lib/nodemailer/templates";
+import {NEWS_SUMMARY_EMAIL_TEMPLATE, WELCOME_EMAIL_TEMPLATE} from "@/lib/nodemailer/templates";
 
 export const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -21,6 +21,24 @@ export const sendWelcomeEmail = async ({ email, name, intro}: WelcomeEmailData) 
         text: 'Thanks for joining Signalist',
         html: htmlTemplate,
     }
+
+    await transporter.sendMail(mailOptions);
+}
+
+export const sendNewsSummaryEmail = async (
+    { email, date, newsContent }: { email: string; date: string; newsContent: string }
+): Promise<void> => {
+    const htmlTemplate = NEWS_SUMMARY_EMAIL_TEMPLATE
+        .replace('{{date}}', email)
+        .replace('{{newsContent}}', newsContent);
+
+    const mailOptions = {
+        from: `"Signalist News" <signalist@jsmastery.pro>`,
+        to: email,
+        subject: `Market News Summary Today - ${date}`,
+        text: `today's market news summary from Signalist`,
+        html: htmlTemplate,
+    };
 
     await transporter.sendMail(mailOptions);
 }
